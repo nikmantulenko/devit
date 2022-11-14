4a1fe6da1085143ed563449e5d285fe05b366deb
nikmant <nikmantulenko@gmail.com>
2022-11-14 18:58:11 +0200

    connected remaining screens to redux

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,15 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import editProfile from '../api/editProfile'
+import { actions, selectors } from '../store';
+import { UserData } from '../types'
+
 export default function Profile(props: NativeStackScreenProps<any>) {
+    const isUpdating = useSelector(selectors.isLoadingSelector('UPDATE_PROFILE'))
+    const error = useSelector(selectors.errorMessageSelector('UPDATE_PROFILE'))
+    const dispatch = useDispatch()
+
+    const userData: UserData = props.route.params as any
+
+    const handleChangeProfile = async () => {
+        !!error && dispatch(actions.removeError('UPDATE_PROFILE'))
+        dispatch(actions.loaderOn('UPDATE_PROFILE'))
+        try {
+            const newUserData = await editProfile({
+                ...userData,
+                name: /*'error' + */userData.name + '.',
+            })
+            props.navigation.setParams(newUserData)
+        } catch (error: any) {
+            dispatch(actions.addError('UPDATE_PROFILE', error.message))
+        } finally {
+            dispatch(actions.loaderOff('UPDATE_PROFILE'))
+        }
+    }
+
     return (
         <>
             <StatusBar style="auto" />
 
             <View style={styles.container}>
                 <Text>PROFILE</Text>
-                <Text>{JSON.stringify(props.route.params)}</Text>
+                {!!error && <Text style={styles.errorMessage}>{error}</Text>}
+                {isUpdating && <ActivityIndicator color={'darkblue'} />}
+                <Text>{JSON.stringify(userData)}</Text>
+                <Button color={'tomato'} title={'edit profile'} onPress={handleChangeProfile} />
                 <Button title={'mock logout'} onPress={() => props.navigation.navigate('log-in')} />
             </View>
         </>
@@ -22,5 +52,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    errorMessage: {
+        color: 'darkred',
     },
 });
--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import apiRegister from '../api/register'
 import apiPhoneCheck from '../api/phoneCheck'
 import apiRequestPhoneCheck from '../api/requestPhoneCheck'
+import { actions, selectors } from '../store';
 
 export default function Register(props: NativeStackScreenProps<any>) {
+    const isRequestingCheck = useSelector(selectors.isLoadingSelector('REQUEST_PHONE_CHECK'))
+    const isChecking = useSelector(selectors.isLoadingSelector('PHONE_CHECK'))
+    const isSigningIn = useSelector(selectors.isLoadingSelector('REGISTRATION'))
+    const requestCheckError = useSelector(selectors.errorMessageSelector('REQUEST_PHONE_CHECK'))
+    const checkError = useSelector(selectors.errorMessageSelector('PHONE_CHECK'))
+    const registrationError = useSelector(selectors.errorMessageSelector('REGISTRATION'))
+    const dispatch = useDispatch()
+
     const [status, setStatus] = useState('INITIAL')
 
+    const error = requestCheckError || checkError || registrationError
+    const loading = isRequestingCheck || isChecking || isSigningIn
     const registerData = {
         name: 'testname',
         email: 'test@gmail.com',
@@ -17,15 +29,45 @@ export default function Register(props: NativeStackScreenProps<any>) {
         phone: '+3801111111111',
     }
 
-    const handleRequestPhoneCheck = () => apiRequestPhoneCheck(registerData.phone)
-        .then(() => setStatus('REQUESTED'))
-        .catch(() => setStatus('ERROR'))
-    const handlePhoneCheck = () => apiPhoneCheck(registerData.phone, '1111')
-        .then(() => setStatus('CONFIRMED'))
-        .catch(() => setStatus('ERROR'))
-    const handleRegister = () => apiRegister(registerData)
-        .then(userData => props.navigation.navigate('profile', userData))
-        .catch(() => setStatus('ERROR'))
+    const handleRequestPhoneCheck = async () => {
+        !!requestCheckError && dispatch(actions.removeError('REQUEST_PHONE_CHECK'))
+        dispatch(actions.loaderOn('REQUEST_PHONE_CHECK'))
+        try {
+            await apiRequestPhoneCheck(registerData.phone)
+            setStatus('REQUESTED')
+        } catch (error: any) {
+            dispatch(actions.addError('REQUEST_PHONE_CHECK', error.message))
+        } finally {
+            dispatch(actions.loaderOff('REQUEST_PHONE_CHECK'))
+        }
+    }
+
+    const handlePhoneCheck = async () => {
+        !!checkError && dispatch(actions.removeError('PHONE_CHECK'))
+        dispatch(actions.loaderOn('PHONE_CHECK'))
+        try {
+            await apiPhoneCheck(registerData.phone, '1111')
+            setStatus('CONFIRMED')
+        } catch (error: any) {
+            dispatch(actions.addError('PHONE_CHECK', error.message))
+        } finally {
+            dispatch(actions.loaderOff('PHONE_CHECK'))
+        }
+    }
+
+    const handleRegister = async () => {
+        !!registrationError && dispatch(actions.removeError('REGISTRATION'))
+        dispatch(actions.loaderOn('REGISTRATION'))
+        try {
+            const userData = await apiRegister(registerData)
+            props.navigation.navigate('profile', userData)
+            setStatus('CONFIRMED')
+        } catch (error: any) {
+            dispatch(actions.addError('REGISTRATION', error.message))
+        } finally {
+            dispatch(actions.loaderOff('REGISTRATION'))
+        }
+    }
 
     return (
         <>
@@ -33,6 +75,8 @@ export default function Register(props: NativeStackScreenProps<any>) {
 
             <View style={styles.container}>
                 <Text>REGISTER SCREEN</Text>
+                {!!error && <Text style={styles.errorMessage}>{error}</Text>}
+                {loading && <ActivityIndicator color={'darkblue'} />}
                 <Button
                     color={'tomato'}
                     title={'request check'}
@@ -63,5 +107,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    errorMessage: {
+        color: 'darkred',
     },
 });
