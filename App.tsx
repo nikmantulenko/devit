d5de88220dd45a259187cb41aeab222d3b56410a
nikmant <nikmantulenko@gmail.com>
2022-11-16 13:52:10 +0200

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import { Provider as StoreProvider } from 'react-redux'
+import { useFonts } from 'expo-font';
 
 import Navigation from './Navigation';
 import store from './store';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+      'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+      'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+      return null
+  }
+
   return (
       <StoreProvider store={store}>
           <Navigation />
