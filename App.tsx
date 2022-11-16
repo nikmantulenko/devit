943b6a5bfcb8804cbb962bf8d951b0fde2d14e27
nikmant <nikmantulenko@gmail.com>
2022-11-16 18:57:52 +0200

    better handling for safe area on top

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { Provider as StoreProvider } from 'react-redux'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { KeyboardAvoidingView, Platform } from 'react-native';
-import { useFonts } from 'expo-font';
 
+import { useFonts } from 'expo-font';
 import Navigation from './Navigation';
 import store from './store';
+
 import globalStyles from './globalStyles';
 
 export default function App() {
@@ -18,12 +20,16 @@ export default function App() {
 
   return (
       <StoreProvider store={store}>
-          <KeyboardAvoidingView
-              style={globalStyles.flex1}
-              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-          >
-              <Navigation />
-          </KeyboardAvoidingView>
+          <SafeAreaProvider>
+              <KeyboardAvoidingView
+                  style={globalStyles.flex1}
+                  behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              >
+                  <SafeAreaView style={globalStyles.flex1} edges={['top']}>
+                      <Navigation />
+                  </SafeAreaView>
+              </KeyboardAvoidingView>
+          </SafeAreaProvider>
       </StoreProvider>
   );
 }
